cec011a224291607c5cd09338164410ab0dddde3
laura <i22032@hb.dhbw-stuttgart.de>
2025-05-27 22:51:14 +0200

    added more tests

---
+++ tests/test_scanning_state.py
@@ -0,0 +1,65 @@
+import unittest
+from data_structures.scanning_state import ScanningState
+from data_structures.token import Token, TokenType
+from data_structures.meta_data import MetaData, Language
+
+class TestScanningState(unittest.TestCase):
+
+    def setUp(self):
+        self.meta_data = MetaData()
+        self.state = ScanningState(token_list=[], meta_data=self.meta_data, remaining_name="John Doe")
+
+    def test_has_first_name_true(self):
+        self.state.token_list.append(Token(TokenType.FIRST_NAME, "John"))
+        self.assertTrue(self.state.has_first_name())
+
+    def test_has_first_name_false(self):
+        self.state.token_list.append(Token(TokenType.LAST_NAME, "Doe"))
+        self.assertFalse(self.state.has_first_name())
+
+    def test_has_salutation_true(self):
+        self.state.token_list.append(Token(TokenType.SALUTATION, "Mr"))
+        self.assertTrue(self.state.has_salutation())
+
+    def test_has_salutation_false(self):
+        self.state.token_list.append(Token(TokenType.FIRST_NAME, "John"))
+        self.assertFalse(self.state.has_salutation())
+
+    def test_update_adds_token_and_updates_remaining_name(self):
+        self.state.update(Token(TokenType.FIRST_NAME, "John"), "Doe", Language.EN)
+        self.assertEqual(len(self.state.token_list), 1)
+        self.assertEqual(self.state.token_list[0].value, "John")
+        self.assertEqual(self.state.remaining_name, "Doe")
+        self.assertEqual(self.state.meta_data.language, Language.EN)
+
+    def test_update_sets_gender(self):
+        self.state.update(Token(TokenType.FIRST_NAME, "John"), "Doe", gender="Männlich")
+        self.assertEqual(self.state.meta_data.gender, "Männlich")
+
+    def test_update_multiple_salutations_raises(self):
+        self.state.token_list.append(Token(TokenType.SALUTATION, "Mr"))
+        with self.assertRaises(ValueError):
+            self.state.update(Token(TokenType.SALUTATION, "Mrs"), "John Doe")
+
+    def test_get_first_name(self):
+        self.state.token_list.append(Token(TokenType.FIRST_NAME, "John"))
+        self.state.token_list.append(Token(TokenType.FIRST_NAME, "Michael"))
+        self.assertEqual(self.state.get_first_name(), "John Michael")
+
+    def test_get_first_name_empty(self):
+        self.assertEqual(self.state.get_first_name(), "")
+
+    def test_get_name(self):
+        self.state.token_list.append(Token(TokenType.SALUTATION, "Mr"))
+        self.state.token_list.append(Token(TokenType.FIRST_NAME, "John"))
+        self.state.token_list.append(Token(TokenType.LAST_NAME, "Doe"))
+        self.assertEqual(self.state.get_name(), "Mr John Doe")
+
+    def test_create_contact(self):
+        self.state.token_list.append(Token(TokenType.FIRST_NAME, "John"))
+        contact = self.state.create_contact()
+        self.assertEqual(contact.token_list[0].value, "John")
+        self.assertIs(contact.meta_data, self.state.meta_data)
+
+if __name__ == "__main__":
+    unittest.main()
