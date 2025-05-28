f3ee2b3b5eadc169b55afb44114985bcaeadcfb2
JonathanFleischmann <i22006@hb.dhbw-stuttgart.degit config --global user.name>
2025-05-28 12:41:18 +0200

    edit options less high

--- user_interface/ui_elements/frame.py
+++ user_interface/ui_elements/frame.py
@@ -1,14 +1,17 @@
 import tkinter as tk
 
 class Frame:
-    def __init__(self, parent, text=None):
+    def __init__(self, parent, text=None, small_margin=False):
         self.parent = parent
         self.text = text
         if text:
             self.frame = tk.LabelFrame(parent, text=text)
         else:
             self.frame = tk.Frame(parent)
-        self.frame.pack(padx=10, pady=10)
+        if small_margin:
+            self.frame.pack(padx=2, pady=2)
+        else:      
+            self.frame.pack(padx=10, pady=10)
 
     def clear(self):
         for widget in self.frame.winfo_children():
